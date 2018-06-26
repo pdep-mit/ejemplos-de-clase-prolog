81ad925e8bc4f1b66158607deb1815cbeb1c6728
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2018-06-26 19:21:56 -0300

    Agrego los predicados a definir durante la clase

--- clase2.pl
+++ clase2.pl
@@ -1,3 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% La familia Simpson
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Repaso de la clase anterior
+%% Arrancamos con esta base de conocimiento
+
 padre(abraham, herbert).
 padre(abraham, homero).
 padre(mona, homero).
@@ -22,3 +29,16 @@ edad(selma, 45).
 edad(lisa, 8).
 edad(bart, 10).
 edad(maggie, 1).
+
+%% Definir los siguientes predicados:
+
+% hermanos/2: dos personas que tienen el mismo padre
+% mellizos/2: hermanos de la misma edad
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% hijoUnico/1: alguien que no tiene hermanos
+% abueloCompleto/1: todos sus hijos tienen hijos
+% tieneCanasVerdes/1: todos sus hijos tienen menos de 15 años
+% esHijoMenor/1: personas que, sin ser hijos únicos, sólo tienen hermanos mayores
