80cf768af26c8aa7937a28f33ab7911aa442701e
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-06-24 11:05:47 -0300

    Agrego cambios pre clase

--- clase2.pl
+++ clase2.pl
@@ -29,6 +29,27 @@ irremplazable(Persona):-
   not((programaEn(Alguien, Lenguaje),
           Alguien \= Persona)).
 
+% No es inversible, porque llega Alguien sin ligar al \=/2
+irremplazableRoto1(Persona):-
+  programaEn(Persona, Lenguaje),
+  not((Alguien \= Persona,
+     programaEn(Alguien, Lenguaje))).
+
+% No es inversible, porque llegan Lenguaje y Persona sin ligar al not/1
+% Esto no anda bien ni para consultas individuales, porque no sabemos cuál es el Lenguaje
+irremplazableRoto2(Persona):-
+  not((programaEn(Alguien, Lenguaje),
+          Alguien \= Persona)),
+  programaEn(Persona, Lenguaje).
+
+% Otro error común: consultar Alguien \= Persona después del not/1
+% No tiene sentido porque not/1 no liga variables, y queda una contradicción
+% al consultar que programaEn(Persona, Lenguaje) y luego negar que alguien programe en ese lenguaje.
+irremplazableRoto3(Persona):-
+  programaEn(Persona, Lenguaje),
+  not(programaEn(Alguien, Lenguaje)),
+  Alguien \= Persona.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Lechuzas mafiosas
@@ -55,5 +76,5 @@ Lo es si su violencia es mayor a 100.
 
 
 /*
-3. Si una lechuza es mafiosa, que se cumple si no es buena gente o su sospechosidad es al menos 75. Decimos que es buena gente si no es vengativa y tiene su nobleza es mayor a 50.
+3. Si una lechuza es mafiosa, que se cumple si no es buena gente o su sospechosidad es al menos 75. Decimos que es buena gente si no es vengativa y su nobleza es mayor a 50.
 */
